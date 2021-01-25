42ff43f8255fae0cf567e77cb8487c3c83ac477f
Van_nya <van114514@outlook.com>
2021-01-25 21:54:00 +0800

    rua

---
+++ JoinMOTDR.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+PLUGIN_METADATA = {
+    'id': 'join_motd_reforged',
+    'version': '1.0.0',
+    'name': 'JoinMOTDR',
+    'description': '',
+    'author': [
+        'Van_Involution',  # Reforged to fit MCDR 1.x
+        'Alex3236'  # Source of inspiration
+    ],
+    'link': 'https://github.com/Van-Involution/MCDR-Plugins',
+    'dependencies': {
+        'mcdreforged': '>=1.0.0',
+    }
+}
