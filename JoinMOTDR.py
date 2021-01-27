0e968ee569ac9f8ed0f3614a70592e614e7d0bf5
Van_nya <van114514@outlook.com>
2021-01-27 23:37:51 +0800

    Almost to be finished

--- JoinMOTDR.py
+++ JoinMOTDR.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 
+import re
 from yaml import load, FullLoader
 from pathlib import Path
 
@@ -25,13 +26,27 @@ VERSION = PLUGIN_METADATA['version']
 DEFAULT_CONFIG_PATH = f'config/{NAME}.yml'
 DEFAULT_CONFIG = '''# Configure file for DayCountR
 
-# 
-# 
-daycount: false
-
-# 
-# 
-
+welcome_message: Welcome, §6{player_name}§r!
+show_daycount: false
+show_seed: false
+show_help: true
+help_message: "§7>>> Click for help message <<<§r"
+show_servers: false
+servers:
+  survival:
+    name: Survival
+    motd: You are now in server §a§l{server_name}§r
+    current: true
+  creative:
+    name: Creative
+    motd: Click to join server §b{server_name}§r
+  mirror:
+    name: Mirror
+    motd: Click to join server §b{server_name}§r
+  qmirror:
+    name: qMirror
+    motd: Click to join server §b{server_name}§r
+current_server: survival
 '''
 
 
@@ -49,16 +64,85 @@ def get_day_count(server: ServerInterface):
     return day_count_r.format_reply_msg(server)
 
 
-def format_reply_msg(server: ServerInterface):
+def get_seed(server: ServerInterface):
+    seed_r = server.get_plugin_instance('seed_reforged')
+    return seed_r.get_seed(server)
+
+
+def get_help_msg(help_msg: str = '>>>Click to get help message<<<'):
+    return RText(help_msg).c(RAction.run_command, '!!help').h('!!help')
+
+
+def get_sub_servers(sub_servers: dict):
+    sub_server_list = []
+    for item in sub_servers:
+        if sub_servers[item].get('current', False):
+            sub_server_list.append(RTextList(
+                '[',
+                RText(sub_servers[item]['name'], RColor.red, RStyle.bold)
+                .h(RText(sub_servers[item]['motd'].format(server_name=sub_servers[item]['name']))),
+                '] '
+            ))
+        else:
+            sub_server_list.append(RTextList(
+                '[',
+                RText(sub_servers[item]['name'], RColor.gold)
+                .h(RText(sub_servers[item]['motd'].format(server_name=sub_servers[item]['name'])))
+                .c(RAction.run_command, f'/server {item}'),
+                '] '
+            ))
+    return RTextList(*sub_server_list)
+
+
+def format_reply_msg(server: ServerInterface, player: str):
     config = get_config(server)
     reply_msg_list = [
-        '-' * 10 + f'JoinMOTDR v{VERSION}' + '-' * 10, '\n'
+        f'{"-" * 8}JoinMOTDR v{VERSION}{"-" * 8}\n\n',
+        RText(config["welcome_message"].format(player_name=player)), '\n'
     ]
-    if config['daycount']:
-        reply_msg_list.append(get_day_count(server) + '\n')
-    reply_msg_list.append('-' * len(reply_msg_list[0]) + '\n')
+    if config['show_daycount']:
+        reply_msg_list.append(get_day_count(server))
+        reply_msg_list.append('\n')
+    if config['show_seed']:
+        reply_msg_list.append(get_seed(server))
+        reply_msg_list.append('\n')
+    reply_msg_list.append('\n')
+    if config['show_servers']:
+        reply_msg_list.append(get_sub_servers(config['servers']))
+        reply_msg_list.append('\n')
+    if config['show_help']:
+        reply_msg_list.append(get_help_msg(config["help_message"]))
+        reply_msg_list.append('\n')
     return RTextList(*reply_msg_list)
 
 
+def player_is_real(server: ServerInterface, player: str):
+    with open('config.yml', 'r') as mcdr_cfg_yml:
+        mcdr_cfg = load(mcdr_cfg_yml, FullLoader)
+        server_dir: str = mcdr_cfg['working_directory']
+    with open(file=f'{server_dir}\\server.properties', mode='r') as server_prop:
+        for prop_line in server_prop.readlines():
+            if (reeeee := re.match(r'^level-name=(.*)', prop_line)) is not None:
+                level_dir: str = reeeee.group(1)
+                break
+    if Path(carpet_conf_path := f'{server_dir}\\{level_dir}\\carpet.conf').is_file():
+        with open(file=carpet_conf_path, mode='r') as carpet_conf:
+            for conf_line in carpet_conf.readlines():
+                if (reeeee1 := re.match(r'^fakePlayerNamePrefix ([^#]*)', conf_line)) is not None:
+                    prefix = reeeee1.group(1)
+                elif (reeeee2 := re.match(r'^fakePlayerNameSuffix ([^#]*)', conf_line)) is not None:
+                    suffix = reeeee2.group(1)
+        if bool(prefix) or bool(suffix):
+            bot_pattern = r'^' + prefix + r'.+' + suffix + r'$'
+            return bool(re.match(bot_pattern, player))
+        else:
+            server.logger.info(f'Cannot determine whatever player {player} is bot')
+            return True
+
+
 def on_player_joined(server: ServerInterface, player: str, info: Info):
-    server.tell(player, format_reply_msg(server))
+    if player_is_real(server, player):
+        server.tell(player, format_reply_msg(server, player))
+        server.logger.info(f'Succeeded to send MOTD to {player}')
+    else:
+        server.logger.info(f'Sent nothing because player {player} is bot')
