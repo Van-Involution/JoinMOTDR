3937cb34d7f2cdee730a13fbaa635744256d7977
Van_nya <van114514@outlook.com>
2021-02-11 16:10:43 +0800

    v1.0.2

--- JoinMOTDR.py
+++ JoinMOTDR.py
@@ -11,7 +11,7 @@ from mcdreforged.api.rtext import RText, RTextTranslation, RTextList, RAction
 
 PLUGIN_METADATA = {
     'id': 'join_motd_reforged',
-    'version': '1.0.1',
+    'version': '1.0.2',
     'name': 'JoinMOTDR',
     'description': '',
     'author': [
